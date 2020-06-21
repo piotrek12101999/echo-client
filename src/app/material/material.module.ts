22d1587d24ab3663edaffb5c046913b07b5ae5e3
Piotr <piter.gazownik@o2.pl>
2020-06-21 13:19:36 +0200

    displaying logs added

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -4,6 +4,7 @@ import { MatButtonModule } from "@angular/material/button";
 import { MatToolbarModule } from "@angular/material/toolbar";
 import { MatFormFieldModule } from "@angular/material/form-field";
 import { MatInputModule } from "@angular/material/input";
+import { MatCardModule } from "@angular/material/card";
 
 @NgModule({
   declarations: [],
@@ -13,12 +14,14 @@ import { MatInputModule } from "@angular/material/input";
     MatToolbarModule,
     MatFormFieldModule,
     MatInputModule,
+    MatCardModule,
   ],
   exports: [
     MatButtonModule,
     MatToolbarModule,
     MatFormFieldModule,
     MatInputModule,
+    MatCardModule,
   ],
 })
 export class MaterialModule {}
